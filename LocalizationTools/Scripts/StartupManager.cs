16851b3992676625ce6331fe6bacca710d581234
sakurazxq <1031845741@qq.com>
2019-06-25 17:33:41 +0800

    游戏开发

---
+++ LocalizationTools/Scripts/StartupManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartupManager : MonoBehaviour
+{
+    // Use this for initialization，等待GetIsReady()为true后切换为MenuScreen场景
+    private IEnumerator Start()
+    {
+        while (!LocalizationManager.instance.GetIsReady())
+        {
+            yield return null;
+        }
+
+        SceneManager.LoadScene("MenuScreen");
+    }
+}

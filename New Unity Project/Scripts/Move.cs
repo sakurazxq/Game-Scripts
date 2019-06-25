691f4974f02fa339ec11ccf40e1b81a1acbe4309
sakurazxq <1031845741@qq.com>
2019-06-25 17:33:41 +0800

    游戏开发

---
+++ New Unity Project/Scripts/Move.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Move : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+}

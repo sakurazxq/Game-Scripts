33a9a723d51cdee66af638452fc07a7d159ed630
sakurazxq <37356764+sakurazxq@users.noreply.github.com>
2019-07-05 16:30:52 +0800

    Add files via upload
    添加UI部分代码

---
+++ UI/Scripts/ControlSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlSound : MonoBehaviour
+{
+    public AudioSource sound;
+    public Slider sd;
+
+    public void Con_Sound()
+    {
+        sound.volume = sd.value;
+    }
+}

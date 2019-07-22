141157063b1f51108694d45b2dbf0cda4358c577
sakurazxq <37356764+sakurazxq@users.noreply.github.com>
2019-07-22 10:52:06 +0800

    物理脚本

---
+++ 物理/3D物理系统/HoverPad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverPad : MonoBehaviour
+{
+    public float hoverForce;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Object entered the trigger");
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        other.GetComponent<Rigidbody>().AddForce(Vector3.up * hoverForce, ForceMode.Acceleration);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Object exited the trigger");
+    }
+}
---
+++ 物理/3D物理系统/RayCasting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCasting : MonoBehaviour
+{
+    public float distance;
+
+    void Update()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, Vector3.down * distance);
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, distance))
+        {
+            Destroy(hit.collider.gameObject);
+        }
+    }
+}

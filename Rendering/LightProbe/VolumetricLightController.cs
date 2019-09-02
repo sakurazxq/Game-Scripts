611a4db436f4e17a88d6dbca7104f74390f0ee31
sakurazxq <1031845741@qq.com>
2019-09-02 10:58:37 +0800

    Rendering

---
+++ Rendering/LightProbe/VolumetricLightController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumetricLightController : MonoBehaviour
+{
+    public VolumetricLight vLight;
+    public float targetScatter;
+
+    // Update is called once per frame
+    void Update()
+    {
+        vLight.ScatteringCoef = Mathf.Lerp(vLight.ScatteringCoef, targetScatter, 0.1f);
+    }
+}
---
+++ Rendering/LightProbe/VolumetricLightTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumetricLightTrigger : MonoBehaviour
+{
+    public VolumetricLightController vLightCon;
+
+    void OnTriggerEnter()
+    {
+        vLightCon.targetScatter = 1.0f;
+    }
+
+    void OnTriggerExit()
+    {
+        vLightCon.targetScatter = 0.0f;
+    }
+}

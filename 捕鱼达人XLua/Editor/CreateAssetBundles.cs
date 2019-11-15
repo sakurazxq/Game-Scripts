d57329fa266d7ac8a7eb1dc4ee4d6470f1adc72a
sakurazxq <1031845741@qq.com>
2019-11-15 16:36:13 +0800

    用XLua实现热更新

---
+++ 捕鱼达人XLua/Editor/CreateAssetBundles.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()      //自动遍历AssetBundles
+    {
+        string dir = "AssetBundles";
+        if (Directory.Exists(dir) == false)
+        {
+            Directory.CreateDirectory(dir);   //在根目录下创建AssetBundles文件夹
+        }
+        BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);   //目标平台是win64位
+    }
+}
---
+++ 捕鱼达人XLua/Scripts/HotFixScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+using System.IO;
+using UnityEngine.Networking;
+
+public class HotFixScript : MonoBehaviour
+{
+    private LuaEnv luaEnv;
+    public static Dictionary<string, GameObject> prefabDict = new Dictionary<string, GameObject>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        luaEnv = new LuaEnv();
+        luaEnv.AddLoader(MyLoader);
+        luaEnv.DoString("require 'fish'");
+    }    
+
+    private byte[] MyLoader(ref string filePath)
+    {
+        string absPath = @"F:\GameResources\CatchFish\FishingJoy\Assets\" + filePath + ".lua.txt";
+        return System.Text.Encoding.UTF8.GetBytes(File.ReadAllText(absPath));
+    }
+
+    private void OnDisable()
+    {
+        luaEnv.DoString("require 'fishDispose'");
+    }
+
+    private void OnDestroy()
+    {
+        luaEnv.Dispose();
+    }
+    [LuaCallCSharp]
+    public  void LoadResource(string resName,string filePath)
+    {
+        StartCoroutine(LoaderResourceCorotine(resName,filePath));
+       
+    }
+
+    IEnumerator LoaderResourceCorotine(string resName,string filePath)
+    {
+        UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(@"http://localhost/AssetBundles/" + filePath);
+        yield return request.SendWebRequest();
+        AssetBundle ab = DownloadHandlerAssetBundle.GetContent(request);
+        GameObject gameObject = ab.LoadAsset<GameObject>(resName);
+        prefabDict.Add(resName, gameObject);
+    }
+
+    [LuaCallCSharp]
+    public static GameObject GetGameObject(string goName)
+    {
+        return prefabDict[goName];
+    }
+}
